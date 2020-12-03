fe930a147a79891da83ae691d1692f8d01c0d78a
Pedgongo <45771748+Pedgongo@users.noreply.github.com>
2020-12-03 21:34:01 +0100

    Finished project

--- interviewApp/src/app/pages/info/info.page.ts
+++ interviewApp/src/app/pages/info/info.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RequestApiService } from 'src/app/services/request-api.service';
 
 @Component({
   selector: 'app-info',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoPage implements OnInit {
 
-  constructor() { }
+  information = null;
+
+  constructor(private activatedRoute: ActivatedRoute, private requestApi: RequestApiService) { }
 
   ngOnInit() {
+
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.requestApi.getData(id).subscribe( result => {
+      this.information = result;
+    })
   }
 
 }
